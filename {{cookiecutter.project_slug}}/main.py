c774743c319c9b2b51a8ab96e66f9a10fd2c3fb4
Jonas Teufel <jonseb1998@gmail.com>
2021-11-27 08:20:44 +0100

    added __init__.py

--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -3,8 +3,9 @@ import sys
 # Plugins can obviously import from ufotest itself
 from ufotest.hooks import Action, Filter
 
-# Since the whole plugin system uses a lot of python dynamic import magic, importing from other modules
-# within the same plugin IS POSSIBLE but should be done as a relative import like this:
+# Since the whole plugin system uses a lot of python dynamic import magic, importing
+# from other modules within the same plugin IS POSSIBLE but should be done as a
+# relative import like this:
 from .util import print_hello_world
 
 
