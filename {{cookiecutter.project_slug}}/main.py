96c9a1b756105c99e54c346987a7a42eca9915da
Jonas Teufel <jonseb1998@gmail.com>
2021-11-29 13:35:28 +0100

    fixed the docker compose path and python import style

--- {{cookiecutter.project_slug}}/main.py
+++ {{cookiecutter.project_slug}}/main.py
@@ -4,13 +4,13 @@ import sys
 from ufotest.hooks import Action, Filter
 
 # Since the whole plugin system uses a lot of python dynamic import magic, importing
-# from other modules within the same plugin IS POSSIBLE but should be done as a
-# relative import like this:
-from .util import print_hello_world
+# from other modules within the same plugin IS POSSIBLE but has to be done as an
+# absolute import, which uses the plugin name like this:
+from {{cookiecutter.project_slug}}.util import print_hello_world
 
 
 @Action('pre_command', 10)
-def hello_world():
+def hello_world(config, namespace, context):
     print_hello_world()
     sys.exit(0)
 
